bf0ad7452459bfa3542d4d8ea779e947c7bbbd59
Scott Macpherson <scottmacphersonmusic@gmail.com>
2016-03-31 16:44:14 -0700

    Refactor transcription spec

--- spec/features/transcription_spec.rb
+++ spec/features/transcription_spec.rb
@@ -6,7 +6,7 @@ describe 'transcriptions' do
       Transcription.create!(song_title: 'Oleo')
       Transcription.create!(song_title: 'Daahoud')
 
-      visit root_path
+      visit transcriptions_path
 
       expect(page).to have_content 'Oleo'
       expect(page).to have_content 'Daahoud'
@@ -14,44 +14,35 @@ describe 'transcriptions' do
   end
 
   context 'show' do
+    before do
+      @oleo = Transcription.create!(
+        song_title: 'Oleo',
+        pdf: 'oleo_coltrane.pdf',
+        mp3: 'oleo_coltrane.mp3'
+      )
+
+      visit root_path
+      click_on 'Oleo'
+    end
+
     it 'should provide a link to view pdf full-screen in a new tab' do
-      Transcription.create!(
-      song_title: 'Oleo',
-      pdf: 'oleo_coltrane.pdf')
+      full_screen_link = page.find(:xpath, '//a', text: 'View Full Screen')
 
-    visit root_path
-    click_on 'Oleo'
+      expect(full_screen_link[:href]).to match(/pdfjs\/full\?file=.*oleo_coltrane/)
+      expect(full_screen_link[:target]).to match(/_blank/)
 
-    full_screen_link = page.find(:xpath, '//a', text: 'View Full Screen')
-    expect(full_screen_link[:href]).to match(/pdfjs\/full\?file=.*oleo_coltrane/)
-    expect(full_screen_link[:target]).to match(/_blank/)
+      full_screen_link.click
 
-    full_screen_link.click
-
-    expect(page).to have_xpath('//body[@id = "pdfjs_viewer-full"]')
+      expect(page).to have_xpath('//body[@id = "pdfjs_viewer-full"]')
     end
 
     it 'should display a pdf of the transcription' do
-    Transcription.create!(
-      song_title: 'Oleo',
-      pdf: 'oleo_coltrane.pdf')
+      pdf_viewer = page.find(:xpath, '//iframe')
+      expect(pdf_viewer[:src]).to match(/pdfjs\/minimal\?file=.*oleo_coltrane/)
+    end
 
-    visit root_path
-    click_on 'Oleo'
-
-    pdf_viewer = page.find(:xpath, '//iframe')
-    expect(pdf_viewer[:src]).to match(/pdfjs\/minimal\?file=.*oleo_coltrane/)
-  end
-
-  it 'should have a basic audio player' do
-    oleo = Transcription.create!(
-      song_title: 'Oleo',
-      mp3: 'oleo_coltrane.mp3'
-    )
-
-    visit transcription_path(oleo)
-
-    expect(page).to have_xpath('//audio[@src = "/audios/oleo_coltrane.mp3"]')
-  end
+    it 'should have a basic audio player' do
+      expect(page).to have_xpath('//audio[@src = "/audios/oleo_coltrane.mp3"]')
+    end
   end
 end
